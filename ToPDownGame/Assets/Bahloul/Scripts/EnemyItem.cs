bdd02013507ad857255b7238be948d08e5936267
mohamedbahloul <53511257+mohamedbahloul@users.noreply.github.com>
2021-07-06 22:48:17 +0100

    animations

---
+++ ToPDownGame/Assets/Bahloul/Scripts/EnemyItem.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+[CreateAssetMenu(fileName ="New Enemy", menuName ="Enemies")]
+public class EnemyItem : ScriptableObject
+{
+    public float speed;
+    public float attackValue;
+   
+}
