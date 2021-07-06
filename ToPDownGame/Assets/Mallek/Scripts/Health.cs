64da05e591613bd37426b95be5f4a13507de48fa
medmalleek <medmallek6@gmail.com>
2021-07-06 23:34:15 +0200

    startHelthBar

---
+++ ToPDownGame/Assets/Mallek/Scripts/Health.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    const int maxHealth=100;
+    int corentHelth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        corentHelth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void damege(int value)
+    {
+        corentHelth -= value;
+    }
+}
---
+++ ToPDownGame/Assets/Mallek/Scripts/MovmentControler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovmentControler : MonoBehaviour
+{
+    public CharacterController characterController;
+
+    public float speed=12;
+    public float gravity=-9.8f;
+    public Transform groundCheck;
+    public float groundDistance=0.4f;
+    public LayerMask mask;
+    Vector3 velocity;
+    bool isGrounded;
+
+    public Joystick joystick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, mask);
+        if (isGrounded && velocity.y<0)
+        {
+            velocity.y = -2;
+        }
+
+        float x = -joystick.Vertical;
+        float y = joystick.Horizontal;
+
+        Vector3 move = transform.right * x + transform.forward * y;
+        characterController.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+    }
+}
