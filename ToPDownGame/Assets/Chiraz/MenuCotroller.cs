955715cd7a8a2f9603c232af1ab4a9c7fd7185e8
Shiraz98 <chirazbougoffa3@gmail.com>
2021-09-11 23:09:03 +0100

    ui

---
+++ ToPDownGame/Assets/Chiraz/MenuCotroller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+public class MenuCotroller : MonoBehaviour
+{
+    public RectTransform shopMenu, changechar, weaponsmenu, coins;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void shopbtn()
+    {
+        shopMenu.DOAnchorPos(Vector2.zero, 0.50f);
+
+    }
+    public void weaponbtn()
+    {
+        weaponsmenu.DOAnchorPos(Vector2.zero, 0.50f);
+    }
+
+    public void changecharbtn()
+    {
+        changechar.DOAnchorPos(Vector2.zero, 0.50f);
+    }
+    public void coinshop()
+    {
+        coins.DOAnchorPos(Vector2.zero, 0.50f);
+    }
+    public void closechangechar()
+    {
+        changechar.DOAnchorPos(new Vector2(2800,0), 0.50f);
+    }
+    public void closecoinsshop()
+    {
+        coins.DOAnchorPos(new Vector2(0, 2800), 0.50f);
+    }
+    public void closeshopmenu()
+    {
+        shopMenu.DOAnchorPos(new Vector2(0, 2800), 0.50f);
+    }
+
+}
