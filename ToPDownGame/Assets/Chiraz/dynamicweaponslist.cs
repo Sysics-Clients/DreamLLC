271e142109f79cde737a88d99ba47e2b63ee7755
Shiraz98 <chirazbougoffa3@gmail.com>
2021-09-28 13:59:00 +0100

    commit

---
+++ ToPDownGame/Assets/Chiraz/dynamicweaponslist.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dynamicweaponslist : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject listitemsPrefab;
+    public Transform listitemsHolder;
+    public int WeaponsNbr;
+    void Start() { 
+    for(int i=0; i< WeaponsNbr; i++)
+        {
+            Instantiate(listitemsPrefab, listitemsHolder);
+        }
+    }
+
+}
