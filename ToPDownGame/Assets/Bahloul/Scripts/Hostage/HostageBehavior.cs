7b79e7accfb4faa8b8472382373bafb9f4d3ae7a
mohamed bahloul <53511257+mohamedbahloul@users.noreply.github.com>
2021-10-09 13:52:44 +0100

    setup training

---
+++ ToPDownGame/Assets/Bahloul/Scripts/Hostage/HostageBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HostageBehavior : MonoBehaviour
+{
+    public delegate void ChangeMovement(HostageController.Movement move);
+    public ChangeMovement changeMovement;
+    
+    public delegate void ChangeState(HostageStates.State state);
+    public ChangeState changeState;
+    
+    public delegate void ChangeAnimationSpeed(float speed);
+    public ChangeAnimationSpeed changeAnimationSpeed;
+
+
+}
