014f18a3dfc323e7e934fdf24630808ad56d4253
hakimnjim <37658409+RymHamdi@users.noreply.github.com>
2021-12-15 22:37:38 +0100

    Update

---
+++ ToPDownGame/Assets/FlamethrowerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlamethrowerController : MonoBehaviour
+{
+    public int damage;
+    
+ void OnCollisionStay(Collision collision)
+    {
+       if (collision.gameObject.tag == "Player")
+
+        {
+                collision.gameObject.GetComponent<PlayerBehavior>().damege(damage);
+        }
+    }
+}
