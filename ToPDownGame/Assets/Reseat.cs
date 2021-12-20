ebade266cfdad801cd18696189efbbb3556c3be0
hakimnjim <njim.hakimnjim1992@gmail.com>
2021-12-20 22:35:44 +0100

    fix

---
+++ ToPDownGame/Assets/Reseat.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reseat : MonoBehaviour
+{
+    public ListItems listitemsPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var item in listitemsPrefab.items)
+        {
+            item.state = StateItem.toBuy;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
